d1773201aaae8c9e2e70493eebddfb14724ca6d7
Sunil Chaudhary <sunilchaudhary2304@gmail.com>
2022-01-11 13:42:40 -0600

    Containerize microservices with creating multi-container app using docker-compose

--- Shopping/Shopping.Client/Controllers/HomeController.cs
+++ Shopping/Shopping.Client/Controllers/HomeController.cs
@@ -6,7 +6,6 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Net.Http;
-using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace Shopping.Client.Controllers
--- Shopping/Shopping.Client/Models/Product.cs
+++ Shopping/Shopping.Client/Models/Product.cs
@@ -2,6 +2,7 @@
 {
     public class Product
     {
+
 		public string Id { get; set; }
 		public string Name { get; set; }
 		public string Category { get; set; }
