9b4e99ae037c3b618726d66f7fde0c32fc585390
Sunil Chaudhary <sunilchaudhary2304@gmail.com>
2022-01-12 05:38:55 -0600

    iPhone 13

--- Shopping/Shopping.API/Data/ProductContext.cs
+++ Shopping/Shopping.API/Data/ProductContext.cs
@@ -34,7 +34,7 @@ namespace Shopping.API.Data
             {
                 new Product()
                 {
-                    Name = "IPhone X",
+                    Name = "IPhone 13",
                     Description = "This phone is the company's biggest change to its flagship smartphone in years. It includes a borderless.",
                     ImageFile = "product-1.png",
                     Price = 950.00M,
